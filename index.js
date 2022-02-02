6106c8a7ae7a9cbe28dea6343f354a66ea0eaccd
Kristina Peru <krissy.germanov@gmail.com>
2022-02-02 16:20:30 -0700

    commit tree ea63cd062ca47e630c7730a705765514b2d07f50

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Code your solution here
+// function findMatching1(drivers, str){
+//   let newList = []
+
+//   for (let i = 0; i < drivers.length; i++){
+
+//     if (drivers[i].toLowerCase() == str.toLowerCase()){
+//       newList.push(drivers[i])
+//     } 
+//   }
+
+//   return newList;
+// }
+
+function findMatching(drivers, str){
+  let newList = drivers.filter(driver => str.toLowerCase() == driver.toLowerCase())
+  return newList
+}
+
+function fuzzyMatch(drivers, str){
+  let newList = drivers.filter(driver => str == driver.slice(0, str.length))
+  return newList
+}
+
+function matchName(drivers, str){
+  let newList = drivers.filter(driver => str == driver.name)
+  return newList
+}
+
+// function matchName2(drivers, str){
+//   let newList = drivers.filter(function(driver){
+//     str == driver.name
+//   })
+//   return newList
+// }
+
+// const drivers = [
+//         {
+//           name: 'Bobby',
+//           hometown: 'Pittsburgh' },
+//         {
+//           name: 'Sammy',
+//           hometown: 'New York' } ,
+//         {
+//           name: 'Sally',
+//           hometown: 'Cleveland' },
+//         {
+//           name: 'Annette',
+//           hometown: 'Los Angeles' },
+//         {
+//           name: 'Bobby',
+//           hometown: 'Tampa Bay' }
+//       ];
+
+
+
